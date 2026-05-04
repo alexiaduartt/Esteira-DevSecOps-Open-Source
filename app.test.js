82c9eba40a2dd7f937cf47799efae236603675fe
joaohenrique11z <joaohenriquelfreire@gmail.com>
2026-05-04 16:47:19 -0300

    PBI - 07 - Testes de Rotas e Testes Localmente

--- app.test.js
+++ app.test.js
@@ -1,1 +1,14 @@
-test('Smoke test', () => { expect(true).toBe(true); });
+const request = require('supertest');
+const app = require('./app'); // Isso puxa o seu app.js da imagem anterior
+
+describe('Suite de Testes da PBI-07', () => {
+    
+    test('Task 1: Criar teste específico para rota /health', async () => {
+        const response = await request(app).get('/health');
+        
+        // Validações baseadas no seu app.js
+        expect(response.statusCode).toBe(200);
+        expect(response.body.status).toBe('UP');
+    });
+
+});
